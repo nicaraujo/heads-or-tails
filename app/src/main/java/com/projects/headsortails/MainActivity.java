283a41b74e01dffc292ea5ac970b60929c81d25c
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-08 10:11:18 -0300

    Game logic added

--- app/src/main/java/com/projects/headsortails/MainActivity.java
+++ app/src/main/java/com/projects/headsortails/MainActivity.java
@@ -2,13 +2,31 @@ package com.projects.headsortails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                int n = new Random().nextInt(2);
+                intent.putExtra("numero", n);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/projects/headsortails/ResultActivity.java
+++ app/src/main/java/com/projects/headsortails/ResultActivity.java
@@ -2,13 +2,40 @@ package com.projects.headsortails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private Button button;
+    private ImageView imageView;
+    private int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        button = findViewById(R.id.button);
+        Bundle data = getIntent().getExtras();
+        num = data.getInt("numero");
+        imageView = findViewById(R.id.imageView2);
+        if (num == 0) {
+            imageView.setImageResource(R.drawable.moeda_cara);
+        }
+        else {
+            imageView.setImageResource(R.drawable.moeda_coroa);
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
